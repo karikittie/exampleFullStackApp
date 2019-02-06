4aaa33d4ac856a24ca299533792ae9f3f93d7667
Karis Sponsler <karis.sponsler@gmail.com>
2019-02-05 18:03:05 -0800

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+const outputDirectory = 'dist';
+
+module.exports = {
+  entry: ['babel-polyfill', './src/client/index.js'],
+  output: {
+    path: path.join(__dirname, outputDirectory),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=100000'
+      }
+    ]
+  },
+  devServer: {
+    port: 3000,
+    open: true,
+    proxy: {
+      '/api': 'http://localhost:8080'
+    }
+  },
+};
